0d82f1427fa43486fc520ad2456ea733c7fb617c
David Richard <107142833+Dehrangerz9@users.noreply.github.com>
2024-05-31 00:24:07 -0300

    Initial commit

---
+++ scripts/temas.js
@@ -0,0 +1,19 @@
+// Seleciona o botão pelo ID
+const botaoControleTema = document.getElementById('botao-controle-tema');
+let temaAtual = 1;
+function alterarTema(){
+    const elementosComClasseTema = document.querySelectorAll(`.tema${temaAtual}`)
+    let novoTema = temaAtual + 1;
+    if (novoTema > 3) {
+        novoTema = 1;
+    }
+    elementosComClasseTema.forEach((elemento) => {
+        elemento.classList.replace(`tema${temaAtual}`,`tema${novoTema}`)
+     })
+    temaAtual = novoTema;
+    this.style.gridColumnStart = `${temaAtual}`;
+    this.style.gridColumnEnd = `${temaAtual + 1 }`;
+
+}
+
+botaoControleTema.addEventListener('click', alterarTema);
